32275300d33ccc3084b80e0d8e6af5233bf78ba1
Matthew Green <greenmatthew07@gmail.com>
2025-04-27 17:13:18 -0500

    Adding support for Keep Highest/Lowest operation on dice pools.

--- src/dice_pool.rs
+++ src/dice_pool.rs
@@ -1,19 +1,45 @@
 use std::fmt;
 use super::die::Die;
 
+pub enum Operation {
+    KeepHighest(u32),
+    KeepLowest(u32),
+}
+
 pub struct DicePool {
     pub die: Die,
     pub count: u32,
+    pub op: Option<Operation>
 }
 
 impl DicePool {
     #[must_use]
-    pub const fn new(sides: u32, count: u32) -> Self {
+    pub const fn new(count: u32, sides: u32) -> Self {
         assert!(sides > 0);
         assert!(count > 0);
+        
         Self {
             die: Die::new(sides),
             count,
+            op: None,
+        }
+    }
+
+    #[must_use]
+    pub const fn new_with_op(count: u32, sides: u32, op: Operation) -> Self {
+        assert!(sides > 0);
+        assert!(count > 0);
+        match op {
+            Operation::KeepHighest(keep) | Operation::KeepLowest(keep) => {
+                assert!(keep > 0);
+                assert!(keep <= count);
+            }
+        }
+
+        Self {
+            die: Die::new(sides),
+            count,
+            op: Some(op),
         }
     }
 
@@ -25,6 +51,19 @@ impl DicePool {
 
 impl fmt::Display for DicePool {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}", self.count, self.die)
+
+        match self.count {
+            1 => write!(f, "{}", self.die)?,
+            _ => write!(f, "{}{}", self.count, self.die)?,
+        }
+        
+        if let Some(op) = &self.op {
+            match op {
+                Operation::KeepHighest(n) => write!(f, "kh{n}")?,
+                Operation::KeepLowest(n) => write!(f, "kl{n}")?,
+            }
+        }
+        
+        Ok(())
     }
 }
